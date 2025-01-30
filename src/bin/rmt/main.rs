ca705ab5d295d04903601213ee376c1008095d77
paulc <212168+paulc@users.noreply.github.com>
2025-01-30 19:42:00 +0000

    ws2812_rmt

--- src/bin/rmt/main.rs
+++ src/bin/rmt/main.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use esp_idf_hal::{delay::FreeRtos, gpio::OutputPin, prelude::Peripherals};
 
 use c3zero::rgb::Rgb;
-use c3zero::status_led::Ws2812Rmt;
+use c3zero::ws2812_rmt::Ws2812Rmt;
 
 pub fn main() -> Result<()> {
     esp_idf_hal::sys::link_patches();
@@ -14,8 +14,8 @@ pub fn main() -> Result<()> {
     let mut status_led = Ws2812Rmt::new(led, channel)?;
 
     // infinite rainbow loop at 20% brightness
-    (0..360).cycle().try_for_each(|hue| {
-        FreeRtos::delay_ms(2);
+    (0..360).cycle().step_by(5).try_for_each(|hue| {
+        FreeRtos::delay_ms(1);
         let rgb = Rgb::from_hsv(hue, 100, 20)?;
         status_led.set(rgb)
     })
