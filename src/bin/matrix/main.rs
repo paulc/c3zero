09fdc3a122603390586eddcdad947a124ea84316
paulc <212168+paulc@users.noreply.github.com>
2025-02-09 23:09:40 +0000

    Fix orientation / Draw 8x8 char

--- src/bin/matrix/main.rs
+++ src/bin/matrix/main.rs
@@ -23,26 +23,57 @@ fn main() -> Result<()> {
     let config = TransmitConfig::new().clock_divider(1);
     let tx = TxRmtDriver::new(channel, led, &config)?;
     let mut ws2812 = Ws2812Rmt::new(tx, 64, RgbLayout::Grb);
+    let mut matrix = Ws2812Matrix::new(Orientation::North);
 
+    rotate(&mut matrix, &mut ws2812, 2)?;
+    matrix.set_orientation(Orientation::North);
+
+    let msg = "Hello There";
     loop {
+        message(&mut matrix, &mut ws2812, msg)?;
+        FreeRtos::delay_ms(500);
+    }
+}
+
+fn message(matrix: &mut Ws2812Matrix, ws2812: &mut Ws2812Rmt, msg: &str) -> Result<()> {
+    for (c1, c2) in msg.chars().zip(msg.chars().skip(1)) {
+        for o in 0..8 {
+            matrix.fill(Rgb::new(0, 0, 0));
+            matrix.draw_char(c1, Rgb::new(128, 0, 0), -o);
+            matrix.draw_char(c2, Rgb::new(128, 0, 0), 8 - o);
+            ws2812.set(matrix.iter())?;
+            FreeRtos::delay_ms(100);
+        }
+    }
+    Ok(())
+}
+
+fn rotate(matrix: &mut Ws2812Matrix, ws2812: &mut Ws2812Rmt, count: usize) -> Result<()> {
+    // Draw arrow
+    for p in [(2, 1), (1, 2), (0, 3)] {
+        matrix.set(p, Rgb::new(128, 0, 0));
+    }
+    for p in [(5, 1), (6, 2), (7, 3)] {
+        matrix.set(p, Rgb::new(0, 0, 128));
+    }
+    for y in 0..8 {
+        let c = 16 * y as u8;
+        matrix.set((3, y), Rgb::new(128 - c, c, 0));
+        matrix.set((4, y), Rgb::new(0, c, 128 - c));
+    }
+
+    for _ in 0..count {
         for o in [
             Orientation::North,
             Orientation::East,
             Orientation::South,
             Orientation::West,
         ] {
-            let mut matrix = Ws2812Matrix::new(o);
-            matrix.fill(Rgb::new(0, 0, 0));
+            matrix.set_orientation(o);
+            println!(">> ORIENTATION:: {:?}", o);
             ws2812.set(matrix.iter())?;
             FreeRtos::delay_ms(500);
-
-            println!(">> ORIENTATION:: {:?}", o);
-            matrix.set((0, 0), Rgb::new(255, 0, 0));
-            matrix.set((1, 1), Rgb::new(0, 255, 0));
-            matrix.set((2, 2), Rgb::new(0, 0, 255));
-            matrix.set((0, 4), Rgb::new(63, 127, 63));
-            ws2812.set(matrix.iter())?;
-            FreeRtos::delay_ms(5000);
         }
     }
+    Ok(())
 }
--- src/utils/ws2812_matrix.rs
+++ src/utils/ws2812_matrix.rs
@@ -1,3 +1,5 @@
+use font8x8::{UnicodeFonts, BASIC_FONTS};
+
 use crate::rgb::Rgb;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -28,8 +30,28 @@ impl Ws2812Matrix {
             self.leds[i] = c;
         }
     }
+    pub fn draw_char(&mut self, c: char, colour: Rgb, offset: i8) {
+        if let Some(glyph) = BASIC_FONTS.get(c) {
+            for (y, row) in glyph.iter().enumerate() {
+                for x in 0..8 {
+                    if row & (1 << x) != 0 {
+                        let x1 = x as i8 + offset;
+                        if (0..WIDTH as i8).contains(&x1) {
+                            self.leds[(x1 as usize) + y * HEIGHT] = colour;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn set_orientation(&mut self, orientation: Orientation) {
+        self.orientation = orientation;
+    }
     pub fn set(&mut self, xy: (usize, usize), c: Rgb) {
         self.leds[xy.0 + xy.1 * HEIGHT] = c;
+    }
+    pub fn get(&mut self, xy: (usize, usize)) -> Rgb {
+        self.leds[xy.0 + xy.1 * HEIGHT]
     }
     pub fn iter(&self) -> Ws2812MatrixIterator {
         Ws2812MatrixIterator {
@@ -54,15 +76,18 @@ impl Iterator for Ws2812MatrixIterator<'_> {
                 Orientation::North => self.leds[self.index],
                 Orientation::East => {
                     let (x, y) = (self.index % WIDTH, self.index / WIDTH);
-                    self.leds[(HEIGHT - y - 1) + x * HEIGHT]
+                    let (x1, y1) = (y, WIDTH - x - 1);
+                    self.leds[x1 + (HEIGHT * y1)]
                 }
                 Orientation::South => {
                     let (x, y) = (self.index % WIDTH, self.index / WIDTH);
-                    self.leds[(WIDTH - x - 1) + (HEIGHT - y - 1) * WIDTH]
+                    let (x1, y1) = (WIDTH - x - 1, WIDTH - y - 1);
+                    self.leds[x1 + (HEIGHT * y1)]
                 }
                 Orientation::West => {
                     let (x, y) = (self.index % WIDTH, self.index / WIDTH);
-                    self.leds[y + (WIDTH - x - 1) * HEIGHT]
+                    let (x1, y1) = (WIDTH - y - 1, x);
+                    self.leds[x1 + (HEIGHT * y1)]
                 }
             };
             self.index += 1;
